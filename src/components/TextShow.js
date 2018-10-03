996a2bf5cd4111abc720649aa9ee898383bfeafd
manisha1896 <manisha.4918@gmail.com>
2018-10-03 17:38:35 +0530

    textshow edit

--- src/components/TextShow.js
+++ src/components/TextShow.js
@@ -3,13 +3,68 @@ import React, { Component } from 'react';
 import './TextShow.scss';
 
 class LoginPage extends Component {
-  render() {
-    return (
-      <div className="textshow">
-        <h2 className="typewriter "> testing ka hs gtrs hsi hsn bsh </h2>
-      </div>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.typerwriter = React.createRef();
+        this.sentences = [
+            "an Engineer. ",
+            "an Inventor. ",
+            "a Developer. ",
+            "a Web Designer. "
+          ]
+    }
+    componentDidMount(){
+        this.loop();
+    }
+    write (obj, sentence, i, cb) {
+        if (i != sentence.length) {
+          setTimeout(function () {
+            i++
+            console.log('in timeout', i)
+            obj.innerHTML = sentence.substr(0, i+1) +' <em aria-hidden="true"></em>';
+            write(obj, sentence, i, cb)
+          }, 50)
+        } else {
+          console.log(i)
+          cb()
+        }
+      }
+    erase (obj, cb,i) {
+       var sentence = obj.innerText
+          if (sentence.length != 0) {
+           setTimeout(function () {
+           sentence = sentence.substr(0,sentence.length-1)
+           console.log('in timeout', i)
+           obj.innerText = sentence
+           erase(obj, cb)
+            }, 18/(i*(i/10000000)))
+            } else {
+            obj.innerText = " "
+            cb()
+         }
+        }
+        var typeline = document.querySelector("#typeline")
+
+     writeerase(obj, sentence, time, cb) {
+          write(obj, sentence, 0, function () {
+           setTimeout(function () {
+           erase(obj, cb) }, time) })
+    }
+
+
+    function loop () {
+        var counter = 0;
+        var sentence = sentences[counter % sentences.length]
+        writeerase(typeline, sentence, 1500, loop)
+        counter++
+      }
+    render() {
+        return (
+        <div className="textshow">
+            <h2 className="" ref={this.typerwriter}> testing ka hs gtrs hsi hsn bsh </h2>
+        </div>
+        );
+    }
 }
 
 export default LoginPage;
